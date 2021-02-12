3ba6206c1cad80f7965e1382368700388f08a402
y-21 <bruiny@126.com>
2021-02-12 09:20:06 +0800

    这是第一次提交

---
+++ demo.js
@@ -0,0 +1,14 @@
+/*
+ * @Author: your name
+ * @Date: 2021-02-12 09:09:01
+ * @LastEditTime: 2021-02-12 09:17:25
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \test-demo1\demo.js
+ */
+window.onload = function (){
+    let box = document.getElementsByClassName('box')[0];
+    box.addEventListener('click',() =>{
+        alert('这是测试代码')
+    })
+}
